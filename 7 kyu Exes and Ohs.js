035b3a36a63fbf5bb5f594e3bdf6e003355006c8
Otatoyiwa <face123456789032@gmail.com>
2026-08-03 17:49:31 +0600

    Update 7 kyu Exes and Ohs.js

--- 7 kyu Exes and Ohs.js
+++ 7 kyu Exes and Ohs.js
@@ -1,1 +1,9 @@
 
+function XO(str) {
+  let o = str.split('').filter(el => el == 'o' || el == 'O');
+  let x = str.split('').filter(el => el == 'x' || el == 'X');
+  let result = (o.length == x.length)? true :
+  (o.length !== x.length)? false : true
+  return result
+    //code here
+}
